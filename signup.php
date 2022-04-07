0832e55cef17eab9e93c111fafdbc971f251cb8a
s3650536 <s3650536@student.rmit.edu.au>
2022-04-07 22:55:18 +1000

    created sign up page. Added php to it to connect to db. However the form inputs are not saving into db. Need to fix error

---
+++ signup.php
@@ -0,0 +1,65 @@
+<?  
+    require_once "db_config.php";
+
+   
+
+    if(isset($_POST["signup"])){
+        $email=$_POST["email"];
+        $first_name=$_POST["firstname"];
+        $last_name=$_POST["lastname"];
+        $password=$_POST["pass"];
+
+        $sql = "INSERT INTO users (email, first_name, last_name, pass) 
+        VALUES ('$email','$first_name','$last_name','$password')";
+
+        mysqli_query($link,$sql);
+
+        header("location: index.html");
+    }
+
+?>
+
+<html>
+    <head>
+    
+    <link id='stylecss' type="text/css" rel="stylesheet" href="style.css">
+    </head>
+    <body>
+    <div class="container">
+        <div class="card">
+            <p><h1>Sign Up</h1></p>
+
+            <form method="POST" action="signup.php" >
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="text" class="form-control" name="email" placeholder="Enter your Email">
+                </div>
+                <div class="form-group">
+                    <label for="firstname">First name</label>
+                    <input type="text" class="form-control" name="firstname" placeholder="Enter your first name">
+                </div>
+
+                <div class="form-group">
+                    <label for="lastname">Last name</label>
+                    <input type="text" class="form-control" name="lastname" placeholder="Enter your last name">
+                </div>
+
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" name="pass" placeholder="Enter your Password">
+                </div>
+                <!-- <button type="submit" name = "signup" class="btn btn-primary">Sign up</button> -->
+                <input class="btn" type="submit" name="signup" value="signup">
+            </form>
+
+         </div>
+    </div>
+</body>
+
+
+
+
+
+
+
+</html>
