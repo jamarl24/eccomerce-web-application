1f08359c20787c2492977643dbdf9dcbf5bb4386
s3650536 <s3650536@student.rmit.edu.au>
2022-06-08 13:28:50 +1000

    completed cart on electronic page

--- js/cart.js
+++ js/cart.js
@@ -24,8 +24,20 @@ for(var i = 0; i < addToCartButtons.length; i++){
     button.addEventListener('click', addToCartClicked)
 }
 
+document.getElementsByClassName('btn-purchase')[0].addEventListener('click',
+purchaseClicked)
+
 
 }
+function purchaseClicked(){
+    alert('Thank you for your purchase')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    while(cartItems.hasChildNodes()){
+        cartItems.removeChild(cartItems.firstChild)
+    }
+    updateCartTotal()
+}
+
 
 function addToCartClicked(event){
     var button = event.target
