d91ea6265323da03f1ca58a8313e312d022b6484
s3650536 <s3650536@student.rmit.edu.au>
2022-06-08 13:01:47 +1000

    added cart section in electronics page. Added add to cart js code. deleted cart html static code

--- js/cart.js
+++ js/cart.js
@@ -17,7 +17,63 @@ for(i=0;i<removeCartItemButtons.length;i++){
     var input= quantityInputs[i]
     input.addEventListener('change', quantityChanged)
 }   
+
+var addToCartButtons = document.getElementsByClassName('shop-item-button')
+for(var i = 0; i < addToCartButtons.length; i++){
+    var button = addToCartButtons[i]
+    button.addEventListener('click', addToCartClicked)
 }
+
+
+}
+
+function addToCartClicked(event){
+    var button = event.target
+    var shopItem = button.parentElement.parentElement
+    var title=shopItem.getElementsByClassName('shop-item-title')[0].innerText
+    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+    var imageSrc = shopItem.getElementsByClassName('img')[0].src
+   
+    //console.log(title+price+imageSrc)
+    addItemToCart(title,price,imageSrc)
+    updateCartTotal()
+    
+}
+
+
+
+function addItemToCart(title,price,imageSrc){
+    //document.createElement allows you to create any html element
+    var cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    //make sure user doesnt add the same item to cart
+    var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+    for(var i = 0; i<cartItemNames.length; i++){
+        if(cartItemNames[i].innerText == title){
+            alert('This item is already added to the cart')
+            return
+        }
+    }
+    
+    var cartRowContents = `
+    <div class="cart-item cart-column">
+        <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
+        <span class="cart-item-title">${title}</span>   
+    </div>
+    <span class="cart-price cart-column">${price}</span>
+    <div class="cart-quantity cart-column">
+        <input class="cart-quantity-input" type="number" value="1">
+        <button class="btn btn-danger" type="button">Remove</button>
+    </div>`
+    cartRow.innerHTML=cartRowContents
+    cartItems.append(cartRow)
+    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', 
+    removeCartItem)
+    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', 
+    quantityChanged)
+}
+
 
 function removeCartItem(){
     var buttonClicked=event.target
